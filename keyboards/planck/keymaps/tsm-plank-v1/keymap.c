76612921e9ae1dfe4e8b1c556babf1c1b6f2eeed
thesecretmaster <thesecretmaster@developingtechnician.com>
2021-08-28 14:55:18 -0500

    Replace raise and lower with symbol layers

--- keyboards/planck/keymaps/tsm-plank-v1/keymap.c
+++ keyboards/planck/keymaps/tsm-plank-v1/keymap.c
@@ -80,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,      KC_Q,    KC_W,    KC_E,       KC_R,         KC_T,           KC_Y,    KC_U,        KC_I,       KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,      KC_A,    KC_S,    KC_D,       KC_F,         KC_G,           KC_H,    KC_J,        KC_K,       KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,       KC_V,         KC_B,           KC_N,    KC_M,        KC_COMM,    KC_DOT,  KC_SLSH, KC_ENT ,
-    ARROWS_CTRL, KC_LGUI, KC_LALT, MO(_RAISE), MO(_LOWER),   MO(_VIM_BINDS), KC_SPC,  MO(_NUMPAD), MO(_RAISE), XXXXXXX, XXXXXXX, XXXXXXX
+    KC_TAB,      KC_Q,    KC_W,    KC_E,       KC_R,         KC_T,           KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,      KC_A,    KC_S,    KC_D,       KC_F,         KC_G,           KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,     KC_Z,    KC_X,    KC_C,       KC_V,         KC_B,           KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    ARROWS_CTRL, KC_LGUI, KC_LALT, MO(_LOWER), MO(_RAISE),   MO(_VIM_BINDS), KC_SPC,  MO(_NUMPAD), XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU
 ),
 
 /* Lower
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
+    KC_TILD, _______, _______, _______, _______, _______, _______, _______, KC_LCBR,    KC_RCBR,    _______, _______,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_LPRN,    KC_RPRN,    _______, _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, S(KC_LCBR), S(KC_RCBR), _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______
 ),
 
 /* Raise
@@ -116,10 +116,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,          KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC,       KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP,       KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AUDIO_MUTE, KC_VOLD, KC_VOLU
+    _______, KC_TILD, _______, KC_EXLM, KC_GRV,  _______, _______, KC_ASTR, KC_CIRC, KC_PERC, _______, _______,
+    _______, _______, KC_AMPR, KC_PIPE, KC_UNDS, _______, _______, KC_EQL,  KC_MINS, KC_PLUS, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_HASH, KC_AT,   KC_DLR,  KC_BSLS, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
