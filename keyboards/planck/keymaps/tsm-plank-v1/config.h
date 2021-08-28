82c9f5e9570855e1b9fca8ec85e1c9518a5ab878
thesecretmaster <thesecretmaster@developingtechnician.com>
2021-08-28 10:32:24 -0500

    Inital commit

---
+++ keyboards/planck/keymaps/tsm-plank-v1/config.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PLANCK_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
+
+/*
+ * MIDI options
+ */
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+// Most tactile encoders have detents every 4 stages
+#define ENCODER_RESOLUTION 4
+
+// https://kevl.in/2021/02/1000hz-qmk-polling/
+// https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options
+#define USB_POLLING_INTERVAL_MS 2
+#define QMK_KEYS_PER_SCAN 4
+#define TAPPING_TOGGLE 2
+// #define NO_PRINT
+// #define NO_DEBUG
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
