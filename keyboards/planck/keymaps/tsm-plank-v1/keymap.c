813b4db134954d244872eaa0c83967b82476bc92
thesecretmaster <thesecretmaster@developingtechnician.com>
2021-08-28 12:55:35 -0500

    Misc bugfixes and add bottom left ctrl + gamer arrow toggle

--- keyboards/planck/keymaps/tsm-plank-v1/keymap.c
+++ keyboards/planck/keymaps/tsm-plank-v1/keymap.c
@@ -29,7 +29,8 @@ enum planck_layers {
 };
 
 enum planck_keycodes {
-  QWERTY = SAFE_RANGE
+  QWERTY = SAFE_RANGE,
+  ARROWS_CTRL
 };
 
 // IDEAS:
@@ -79,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,           KC_Y,    KC_U,        KC_I,    KC_O,     KC_P,    KC_BSPC,
-    KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,         KC_G,           KC_H,    KC_J,        KC_K,    KC_L,     KC_SCLN, KC_QUOT,
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,           KC_N,    KC_M,        KC_COMM, KC_DOT,   KC_SLSH, KC_ENT ,
-    TT(_RAISE), KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER),   MO(_VIM_BINDS), KC_SPC,  MO(_NUMPAD), MO(_GAMER_ARROWS), XXXXXXX, XXXXXXX,
+    KC_TAB,      KC_Q,    KC_W,    KC_E,       KC_R,         KC_T,           KC_Y,    KC_U,        KC_I,       KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,      KC_A,    KC_S,    KC_D,       KC_F,         KC_G,           KC_H,    KC_J,        KC_K,       KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,     KC_Z,    KC_X,    KC_C,       KC_V,         KC_B,           KC_N,    KC_M,        KC_COMM,    KC_DOT,  KC_SLSH, KC_ENT ,
+    ARROWS_CTRL, KC_LGUI, KC_LALT, MO(_RAISE), MO(_LOWER),   MO(_VIM_BINDS), KC_SPC,  MO(_NUMPAD), MO(_RAISE), XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Lower
@@ -100,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
 ),
 
 /* Raise
@@ -118,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,          KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC,       KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP,       KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_AUDIO_MUTE, KC_VOLD, KC_VOLU
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AUDIO_MUTE, KC_VOLD, KC_VOLU
 ),
 
 /* Adjust (Lower + Raise)
@@ -152,6 +153,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint16_t my_hash_timer;
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
@@ -160,6 +162,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case ARROWS_CTRL:
+      if (record->event.pressed) {
+         my_hash_timer = timer_read();
+         register_code(KC_LCTRL);
+      } else {
+         unregister_code(KC_LCTRL);
+         if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
+            layer_invert(_GAMER_ARROWS);
+         }
+      }
+      return false;
   }
   return true;
 }
