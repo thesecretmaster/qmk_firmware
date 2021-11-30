d1cf32e733bc9b818ce2685a8226f23396cfd211
thesecretmaster <thesecretmaster@developingtechnician.com>
2021-11-29 20:39:05 -0600

    Bring combos back to what they used to be

--- keyboards/planck/keymaps/tsm-plank-v1/keymap.c
+++ keyboards/planck/keymaps/tsm-plank-v1/keymap.c
@@ -46,7 +46,7 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM gamer_arrow_combo[] = {KC_MUTE, KC_VOLD, KC_VOLU, COMBO_END};
 const uint16_t PROGMEM lgui_combo[] = {KC_LALT, MO(_RAISE), COMBO_END};
-const uint16_t PROGMEM lgui_space_combo[] = {MO(_RAISE), KC_SPC, COMBO_END};
+const uint16_t PROGMEM lgui_space_combo[] = {MO(_RAISE), MO(_NUMPAD), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
    COMBO(gamer_arrow_combo, GAMER_ARROW_ON),
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q, KC_W,       KC_E,     KC_R,    KC_T,       KC_Y,   KC_U,        KC_I,           KC_O,    KC_P,    KC_BSPC,
     KC_ESC,     KC_A, KC_S,       KC_D,     KC_F,    KC_G,       KC_H,   KC_J,        KC_K,           KC_L,    KC_SCLN, KC_QUOT,
     SHIFT_CAPS, KC_Z, KC_X,       KC_C,     KC_V,    KC_B,       KC_N,   KC_M,        KC_COMM,        KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LGUI,    HOLD, MO(_LOWER), KC_LCTRL, KC_LALT, MO(_RAISE), KC_SPC, TT(_NUMPAD), MO(_VIM_BINDS), KC_MUTE, KC_VOLD, KC_VOLU
+    KC_LGUI,    HOLD, MO(_LOWER), KC_LCTRL, KC_LALT, MO(_RAISE), KC_SPC, MO(_NUMPAD), MO(_VIM_BINDS), KC_MUTE, KC_VOLD, KC_VOLU
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
